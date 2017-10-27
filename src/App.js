2ec29994920d63c2d7644b8763a7e9b5bf27622a
katiescruggs <katie.e.scruggs@gmail.com>
2017-10-26 19:00:55 -0600

    add Header and Banner elements

--- src/App.js
+++ src/App.js
@@ -48,6 +48,7 @@ const App = () => {
       <Stories  storiesTitle="Today's Top Stories"
                 stories={stories} />
       {/* add another Stories component with storiesTitle 'Pop Culture', and just pass down the same stories array as the above Stories component (already created but will need some editing) */}
+      
       {/* add a Footer Component (you will need to create this component in the provided Footer.js file) */}
     </div>
   );
